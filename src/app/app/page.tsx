865369554e08f47910b88224ce12617c57cd4bc6
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-02-29 12:03:38 -0300

    feat: add sign out

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,7 +1,12 @@
 import { auth } from "@/services/auth";
+import { UserInfo } from "./_components/user-info";
 
 export default async function Page() {
   const session = await auth();
 
-  return <pre>{JSON.stringify(session?.user, null, 1)}</pre>;
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <UserInfo user={session?.user} />
+    </main>
+  );
 }
