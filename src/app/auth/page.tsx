6916c507740caf9bb2e76b50f6047b50e1bac105
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-03-01 13:28:42 -0300

    fix: import error

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,7 @@
 import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 
-import { Icons } from '@/components'
+import { Icons } from '@/components/icon'
 
 import { AuthForm } from './_components'
 
