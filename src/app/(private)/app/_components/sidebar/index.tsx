903989cfcea330f6ba440e070b7f26500d229a42
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-03-11 11:06:07 -0300

    feat: lint fix

--- src/app/(private)/app/_components/sidebar/index.tsx
+++ src/app/(private)/app/_components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
-import { format, isToday, isTomorrow } from 'date-fns'
+import { format } from 'date-fns'
 import {
   Calendar,
   CalendarDays,
