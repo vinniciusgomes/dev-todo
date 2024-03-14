4f86a4da59b02e3c4aed4ea14752e8ff31274f15
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-03-14 10:57:48 -0300

    fix: envs

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -34,16 +34,6 @@ export const {
     }),
     EmailProvider({
       apiKey: process.env.RESEND_API_KEY,
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT
-          ? +process.env.EMAIL_SERVER_PORT
-          : 587,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
       from: process.env.EMAIL_FROM,
     }),
   ],
