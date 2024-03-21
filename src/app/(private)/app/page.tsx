58668040cc0d894e561e3efbe75a949da3c8ffd9
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-03-21 14:27:25 -0300

    feat: add subdomain cookie

--- src/app/(private)/app/page.tsx
+++ src/app/(private)/app/page.tsx
@@ -1,5 +1,6 @@
 import { getTasks } from '@/actions/task/actions'
 import { Separator } from '@/components/ui/separator'
+import { getUrl } from '@/lib/get-url'
 import { auth } from '@/services/auth'
 
 import { PhraseOfTheDay } from './_components/phrase-of-the-day'
@@ -13,6 +14,8 @@ export default async function Page() {
   const session = await auth()
   const tasks = await getTasks({})
   const sortedTasks = sortByDate(tasks)
+
+  console.log(new URL(getUrl('/app')))
 
   return (
     <div className="flex w-full flex-col gap-10">
