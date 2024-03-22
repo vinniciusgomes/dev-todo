5cb1834995ca8620521ed9273829bf7bc26c3c06
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2024-03-22 10:57:13 -0300

    fix: navigation sheet bug :bug:

--- src/app/(private)/app/_components/navigation-sheet.tsx
+++ src/app/(private)/app/_components/navigation-sheet.tsx
@@ -20,6 +20,7 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet'
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { Tag, Task } from '@/types'
 
 import { SidebarNavItem } from './sidebar/sidebar-nav-item'
@@ -74,104 +75,106 @@ export function NavigationSheet({ tasks, tags }: Props) {
   const trashTasksCount = tasks?.filter((task) => task.deleted).length || 0
 
   return (
-    <SheetContent side="left">
-      <SheetHeader>
-        <SheetTitle className="text-left">DevToDo</SheetTitle>
-      </SheetHeader>
-      <div className="mt-8 grid gap-8">
-        <div>
-          <ul className="mt-2 space-y-1">
-            <SidebarNavItem
-              label="All"
-              onClick={() => push('/app')}
-              active={pathname === '/app'}
-              count={allTasksCount}
-              icon={GalleryVerticalEnd}
-            />
-            <SidebarNavItem
-              label="Today"
-              onClick={() => push('/app/today')}
-              count={todayTasksCount}
-              active={pathname === '/app/today'}
-              icon={Calendar}
-            />
-            <SidebarNavItem
-              label="Tomorrow"
-              onClick={() => push('/app/tomorrow')}
-              count={tomorrowTasksCount}
-              active={pathname === '/app/tomorrow'}
-              icon={Sunrise}
-            />
-            <SidebarNavItem
-              label="Next 7 days"
-              onClick={() => push('/app/next-7-days')}
-              count={nextSevenDaysTasksCount}
-              active={pathname === '/app/next-7-days'}
-              icon={CalendarDays}
-            />
-            <SidebarNavItem
-              label="Completed"
-              onClick={() => push('/app/completed')}
-              count={completedTasksCount}
-              active={pathname === '/app/completed'}
-              icon={CheckSquare}
-            />
-            <SidebarNavItem
-              label="Trash"
-              onClick={() => push('/app/trash')}
-              count={trashTasksCount}
-              active={pathname === '/app/trash'}
-              icon={Trash2}
-            />
-          </ul>
-        </div>
-
-        <div>
-          <h3 className="text-lg font-semibold">Tags</h3>
-
-          <ul className="mt-2 space-y-1">
-            {tags?.map((tag) => (
-              <SidebarTag
-                key={tag.id}
-                label={tag.name}
-                onClick={() =>
-                  push(
-                    `/app/${tag.name.replaceAll(' ', '-').toLocaleLowerCase()}`,
-                  )
-                }
-                active={
-                  pathname ===
-                  `/app/${tag.name.replaceAll(' ', '-').toLocaleLowerCase()}`
-                }
-                count={tag.tasks?.length || 0}
-                color={tag.color}
+    <TooltipProvider>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle className="text-left">DevToDo</SheetTitle>
+        </SheetHeader>
+        <div className="mt-8 grid gap-8">
+          <div>
+            <ul className="mt-2 space-y-1">
+              <SidebarNavItem
+                label="All"
+                onClick={() => push('/app')}
+                active={pathname === '/app'}
+                count={allTasksCount}
+                icon={GalleryVerticalEnd}
               />
-            ))}
+              <SidebarNavItem
+                label="Today"
+                onClick={() => push('/app/today')}
+                count={todayTasksCount}
+                active={pathname === '/app/today'}
+                icon={Calendar}
+              />
+              <SidebarNavItem
+                label="Tomorrow"
+                onClick={() => push('/app/tomorrow')}
+                count={tomorrowTasksCount}
+                active={pathname === '/app/tomorrow'}
+                icon={Sunrise}
+              />
+              <SidebarNavItem
+                label="Next 7 days"
+                onClick={() => push('/app/next-7-days')}
+                count={nextSevenDaysTasksCount}
+                active={pathname === '/app/next-7-days'}
+                icon={CalendarDays}
+              />
+              <SidebarNavItem
+                label="Completed"
+                onClick={() => push('/app/completed')}
+                count={completedTasksCount}
+                active={pathname === '/app/completed'}
+                icon={CheckSquare}
+              />
+              <SidebarNavItem
+                label="Trash"
+                onClick={() => push('/app/trash')}
+                count={trashTasksCount}
+                active={pathname === '/app/trash'}
+                icon={Trash2}
+              />
+            </ul>
+          </div>
 
-            <li>
-              <Button
-                className="mt-4 flex w-full items-center justify-start gap-2 px-2"
-                variant="ghost"
-                onClick={() => push('/settings/tags')}
-              >
-                <Plus className="h-4 w-4" />
-                <span className="text-sm ">Create new tag</span>
-              </Button>
-            </li>
-          </ul>
+          <div>
+            <h3 className="text-lg font-semibold">Tags</h3>
+
+            <ul className="mt-2 space-y-1">
+              {tags?.map((tag) => (
+                <SidebarTag
+                  key={tag.id}
+                  label={tag.name}
+                  onClick={() =>
+                    push(
+                      `/app/${tag.name.replaceAll(' ', '-').toLocaleLowerCase()}`,
+                    )
+                  }
+                  active={
+                    pathname ===
+                    `/app/${tag.name.replaceAll(' ', '-').toLocaleLowerCase()}`
+                  }
+                  count={tag.tasks?.length || 0}
+                  color={tag.color}
+                />
+              ))}
+
+              <li>
+                <Button
+                  className="mt-4 flex w-full items-center justify-start gap-2 px-2"
+                  variant="ghost"
+                  onClick={() => push('/settings/tags')}
+                >
+                  <Plus className="h-4 w-4" />
+                  <span className="text-sm ">Create new tag</span>
+                </Button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <SheetFooter className="absolute bottom-6">
-        <Button
-          variant="text"
-          size="text"
-          className="flex gap-2"
-          onClick={() => push('/settings')}
-        >
-          <Settings className="h-4 w-4" />
-          Settings
-        </Button>
-      </SheetFooter>
-    </SheetContent>
+        <SheetFooter className="absolute bottom-6">
+          <Button
+            variant="text"
+            size="text"
+            className="flex gap-2"
+            onClick={() => push('/settings')}
+          >
+            <Settings className="h-4 w-4" />
+            Settings
+          </Button>
+        </SheetFooter>
+      </SheetContent>
+    </TooltipProvider>
   )
 }
